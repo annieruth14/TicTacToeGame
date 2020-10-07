239a4c154e58887695105ce3718f7fd5247aa3a2
annie <ruthannie3@gmail.com>
2020-10-07 12:27:56 +0530

    [Annie]Refactored: method for exception handling

--- src/com/bridgelabz/game/TicTacToeGame.java
+++ src/com/bridgelabz/game/TicTacToeGame.java
@@ -3,52 +3,59 @@ package com.bridgelabz.game;
 import java.util.Scanner;
 
 public class TicTacToeGame {
-	Scanner sc = new Scanner(System.in);
+	Scanner userInput = new Scanner(System.in);
 	public static TicTacToeGame obj = new TicTacToeGame();
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic tac toe game");
 		char board[] = obj.createBoard();
 		char userLetter = obj.chooseLetter();
 		char computerLetter = (userLetter == 'X') ? 'O' : 'X';
-		System.out.println("User: "+userLetter + " Computer: "+computerLetter);
+		System.out.println("User: " + userLetter + " Computer: " + computerLetter);
 		obj.showBoard(board);
 		char userBoard[] = obj.makeMove(board, userLetter);
 		obj.showBoard(userBoard);
-	} 
-	// UC1
+	}
+
+	// UC1 Creating a board
 	public char[] createBoard() {
 		char board[] = new char[10];
-		for(int i=1; i< board.length; i++) {
-			board[i] = ' ';
+		for (int index = 1; index < board.length; index++) {
+			board[index] = ' ';
 		}
 		return board;
 	}
-	// UC2
+
+	// UC2 Choose a letter
 	public char chooseLetter() {
 		System.out.println("Choose a letter");
-		char letter = sc.next().charAt(0);
+		char letter = userInput.next().charAt(0);
 		return letter;
 	}
-	// UC3
+
+	// UC3 Display the board
 	public void showBoard(char board[]) {
 		System.out.println("Board");
-		System.out.println(" "+board[1]+ " | " + board[2] + " | "+ board[3]);
+		System.out.println(" " + board[1] + " | " + board[2] + " | " + board[3]);
 		System.out.println("-----------");
-		System.out.println(" "+board[4]+ " | " + board[5] + " | "+ board[6]);
+		System.out.println(" " + board[4] + " | " + board[5] + " | " + board[6]);
 		System.out.println("-----------");
-		System.out.println(" "+board[7]+ " | " + board[8] + " | "+ board[9]);
+		System.out.println(" " + board[7] + " | " + board[8] + " | " + board[9]);
 	}
-	// UC4
+
+	// UC4 Make a move
 	public char[] makeMove(char board[], char userLetter) {
-		int flag = 0;
-		System.out.println("Select a position to move: ");
-		int position = sc.nextInt();
-		for(int i=1; i< board.length; i++) {
-			if(board[position] == ' ') {
-				board[position] = userLetter;
-			}	
+		try {
+			System.out.println("Select a position to move: ");
+			int position = userInput.nextInt();
+			for (int index = 1; index < board.length; index++) {
+				if (board[position] == ' ') {
+					board[position] = userLetter;
+				}
+			}
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Index out of range");
 		}
 		return board;
 	}
 }
-
