9d2bf4c5c3797c10265985f27e79fe9a0950c468
annie <ruthannie3@gmail.com>
2020-10-07 10:57:26 +0530

    [Annie]Refactored: code for displaying the board

--- src/com/bridgelabz/game/TicTacToeGame.java
+++ src/com/bridgelabz/game/TicTacToeGame.java
@@ -28,7 +28,13 @@ public class TicTacToeGame {
 	}
 	// UC3
 	public void showBoard(char board[]) {
-		for(int i=0; i<board.length;i++)
-			System.out.println("position: " + i + " Value: "+board[i] );
+		System.out.println("Board");
+		System.out.println("\n "+board[1]+ " | " + board[2] + " | "+ board[3]);
+		System.out.println("-----------");
+		System.out.println(" "+board[4]+ " | " + board[5] + " | "+ board[6]);
+		System.out.println("-----------");
+		System.out.println(" "+board[7]+ " | " + board[8] + " | "+ board[9]);
+
 	}
 }
+
