83b893b349a6b36edeacf72fae5eceabb6a8b2d0
annie <ruthannie3@gmail.com>
2020-10-07 10:37:41 +0530

    [Annie]Added: code for displaying the board

--- src/com/bridgelabz/game/TicTacToeGame.java
+++ src/com/bridgelabz/game/TicTacToeGame.java
@@ -7,19 +7,28 @@ public class TicTacToeGame {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic tac toe game");
 		TicTacToeGame obj = new TicTacToeGame();
-		obj.createBoard();
-		obj.chooseLetter();
+		char board[] = obj.createBoard();
+		char userLetter = obj.chooseLetter();
+		char computerLetter = (userLetter == 'X') ? '0' : 'X';
+		obj.showBoard(board);
 	}
 	// UC1
-	public void createBoard() {
+	public char[] createBoard() {
 		char board[] = new char[10];
 		for(int i=1; i< board.length; i++) {
 			board[i] = ' ';
 		}
+		return board;
 	}
 	// UC2
-	public void chooseLetter() {
+	public char chooseLetter() {
 		System.out.println("Choose a letter");
 		char letter = sc.next().charAt(0);
+		return letter;
+	}
+	// UC3
+	public void showBoard(char board[]) {
+		for(int i=0; i<board.length;i++)
+			System.out.println("position: " + i + " Value: "+board[i] );
 	}
 }
